994b3148d342803582dadf88ae1d68afa2f57b32
Norbert <norbert@wagnerowie.pl>
2021-01-31 04:04:53 +0100

    error handling

--- controllers/savetofile.go
+++ controllers/savetofile.go
@@ -11,7 +11,10 @@ import (
 func Save(encryptedS string) {
 	date := time.Now().String()
 	fileName := "./crypted/crypted-" + strings.TrimSpace(date) + ".txt"
-	file, _ := os.Create(fileName)
+	file, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
 	defer file.Close()
 	ln, _ := io.WriteString(file, encryptedS)
 	fmt.Println(ln)
